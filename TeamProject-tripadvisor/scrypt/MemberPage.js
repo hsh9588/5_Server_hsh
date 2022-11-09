883de9c0a8c77a56bd86c0ad9e22d5e4a016c779
hsh9588 <hsh9588@naver.com>
2022-11-09 20:24:10 +0900

    20221109 수업종료

--- TeamProject-tripadvisor/scrypt/MemberPage.js
+++ TeamProject-tripadvisor/scrypt/MemberPage.js
@@ -1,19 +1,17 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slide");
+    let i;
+    let slides = document.getElementsByClassName("slide");
     if (n > slides.length) {slideIndex = 1} 
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
@@ -21,4 +19,3 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block"; 
 } 
-

da36c54faa109d211d138484c121f16875844fba
hsh9588 <hsh9588@naver.com>
2022-11-10 20:35:05 +0900

    20221110수업종료

--- TeamProject-tripadvisor/scrypt/MemberPage.js
+++ TeamProject-tripadvisor/scrypt/MemberPage.js
@@ -19,3 +19,52 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block"; 
 } 
+
+const dotmenu = document.getElementsByClassName("user-page-review-dot-style");
+
+for(let item of dotmenu){
+    item.addEventListener("click", function(){
+        item.nextElementSibling.style.display = "inline-block";
+    });
+    item.nextElementSibling.addEventListener("click", function(){
+        item.nextElementSibling.style.display = "none";
+    });
+}
+
+const suportbutton = document.getElementsByClassName("suport-button");
+
+for(let item of suportbutton){
+    let count = 0;
+    let flag = false;
+    
+    item.addEventListener("click", function(){
+
+        if(flag == false){
+            item.firstElementChild.style.color = "black";
+            item.style.textDecoration = "underline";
+            count++;
+            flag = true;
+        } else {
+            item.firstElementChild.style.color = "#757575";
+            item.style.textDecoration = "none";
+            count--;
+            flag = false;
+        }
+
+        if (count == 0){
+            item.parentElement.previousElementSibling.style.display = "none";
+            // item.parentElement.previousElementSibling.style.visibility = "hidden";
+
+            // item.parentElement.previousElementSibling.style.maxHeight= "0px";
+        } else {
+            // item.parentElement.previousElementSibling.style.visibility = "visible";
+            // item.parentElement.previousElementSibling.style.maxHeight= "300px";
+            item.parentElement.previousElementSibling.style.display = "flex";
+            item.parentElement.previousElementSibling.innerHTML = count + "개의 도움이 되는 리뷰";
+
+
+            
+        }
+    });
+
+}

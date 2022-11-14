8c7ae5160d2d8c94c586dda4964c8e1ef987dee8
hsh9588 <hsh9588@naver.com>
2022-11-14 19:12:58 +0900

    20221114 수업종료

---
+++ TeamProject-tripadvisor/test/js/test.js
@@ -0,0 +1,10 @@
+function show(){
+    document.querySelector(".profile-page-background").className = "profile-page-popup profile-edit";
+}
+
+function close() {
+    document.querySelector(".profile-page-background").className = "profile-page-popup";
+}
+
+document.querySelector("#profile-edit-show").addEventListener("click", show);
+document.querySelector("#profile-edit-close").addEventListener("click", close);
